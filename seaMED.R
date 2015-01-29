b45d795f2c4f7fa8cbdaa74577d83c6fa10d53c8
neavemj <matthewjneave1@gmail.com>
2015-01-29 09:30:18 +0300

    fixed up arrows for indic species

--- seaMED.R
+++ seaMED.R
@@ -58,16 +58,72 @@ plot_bar(seaFiltGlom, fill="Phylum", x="names") +
 
 # SAVE EPS 1500 x 700
 
+# ordination for the seawater samples
+
+theme_set(theme_bw())
+seaOrd <- ordinate(sea, "NMDS", "bray")
+plot_ordination(sea, seaOrd, type = 'samples', color='site', title='seawater')
+
+# overlay the OTUs onto this
+  
+seaFilt = filter_taxa(sea, function(x) mean(x) > 0.1, TRUE)
+
+#Top10OTUs = names(sort(taxa_sums(seaFilt), TRUE)[1:10])
+#seaFilt10 = prune_taxa(Top10OTUs, seaFilt)
+  
+seaOrd <- ordinate(sea, "NMDS", "bray")
+plot_ordination(sea, seaOrd, type = 'split', color='site', title='seawater', label="Genus")
+
+# calculate indicator species
+  
+seaDist <- vegdist(otu_table(sea), method="bray")
+seaInd <- indspc(otu_table(sea), seaDist, numitr=1000)
+
+# sort by most important indicator species
+  
+seaIndVals <- seaInd$vals
+seaIndValsSorted <- seaIndVals[order(-seaIndVals$indval),,drop=FALSE]
+
+indval numocc  pval
+MED000006898 0.82280849      2 0.027
+MED000001740 0.79870842      2 0.046
+MED000004938 0.78373661      2 0.054
+MED000007874 0.75106607     14 0.001
+MED000005832 0.74325044      2 0.080
+MED000003752 0.73354346      9 0.001
+MED000006835 0.73130608      6 0.001
+MED000002578 0.70127820     22 0.001
+MED000007846 0.70127820     22 0.001
+MED000000600 0.69992476     23 0.001
+
+# overlay these onto ordination
+  
+seaOrdTop10 <- seaOrd$species[c(rownames(seaIndValsSorted)[1:10]),]
+
+arrowmatrix = seaOrdTop10
+arrowdf <- data.frame(labels = rownames(arrowmatrix), arrowmatrix)
+
+# get taxonomic information from the original tax file
+  
+arrowdf <- data.frame(labels = allTax[rownames(arrowmatrix),"Family"], arrowmatrix)
+
+arrowmap <- aes(xend = MDS1, yend = MDS2, x = 0, y = 0, alpha=1, shape = NULL, color = NULL, label = labels)
+labelmap <- aes(x = MDS1, y = MDS2 + 0.04, shape = NULL, color = NULL, label = labels, size=1.5)
+arrowhead = arrow(length = unit(0.02, "npc"))
+
+seaFiltPlot <- plot_ordination(sea, seaOrd, type = 'samples', color='site', title='seawater')
+
+seaFiltPlotArrow <- seaFiltPlot + geom_segment(arrowmap, size = 1, data = arrowdf, color = "black",  arrow = arrowhead, show_guide = FALSE) + geom_text(labelmap, size = 4, data = arrowdf)
+seaFiltPlotArrow
+
 # see if SIMPER works
 
-metaFileSIMP <- subset(metaFile, site != 'NA')
+#metaFileSIMP <- subset(metaFile, site != 'NA')
 seaDistSIMP <- merge(otu_table(sea), metaFileSIMP, by="row.names")
 
 rownames(seaDistSIMP) <- seaDistSIMP[,1]
 OTUsSIMP <- seaDistSIMP[,2:743]
 factorsSIMP <- seaDistSIMP[,744:749]
-
-seaDistSIMP[,750]
 
 spistSIMP <- simper(OTUsSIMP, factorsSIMP$site)
 
